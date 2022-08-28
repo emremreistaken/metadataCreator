531c93214056ac552f5bfad6479002c3076897ae
emremre <emre.guclu@metu.edu.tr>
2022-08-28 23:40:54 +0300

    initial commit

---
+++ metadataCreator.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const collectionSize = 11;
+const imageIpfs = 'ipfs://ipfsCid/';
+const collectionName = 'Some Name';
+const description = 'Description';
+
+for (num = 1; num <= collectionSize; num++) {
+	// creates JSON objects
+	const metadata = {
+		name: `${collectionName} #${num}`,
+		description: `${description}`,
+		image: `${imageIpfs}${num}`
+	};
+
+	// convert JSON object to string
+	const data = JSON.stringify(metadata);
+
+	// write JSON string to a file
+	fs.writeFile(`./metadata/${num}.json`, data, (err) => {
+		if (err) {
+			throw err;
+		}
+		console.log('JSON datas are saved.');
+	});
+}
